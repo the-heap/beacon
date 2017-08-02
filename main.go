a38af863d8340a76b692b68c19b9545cec178a24
Bronek Szulc <bronek@thinkdataworks.com>
2017-08-02 19:53:51 -0400

    reading log file located in dropbox
    Former-commit-id: dd6b17bb006c0dd9f9e29365f8026c38abccb354
    Former-commit-id: acb9dcd537ea6167dde8abc24e7d77991e4294fa

--- main.go
+++ main.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
 )
 
-func main() {
-	fmt.Println("Hello, I'm Beacon!")
+var logFilePath = path.Join(os.Getenv("HOME"), "/Dropbox/The Heap/Beacon/beacon_log.json")
 
+func checkError(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	dat, err := ioutil.ReadFile(path.Join(logFilePath))
+	checkError(err)
+
+	fmt.Print(string(dat))
 }
