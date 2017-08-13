2f3a8218e192c895d1a217ac75a215b4b21cb6fd
teesloane <tylersloane@gmail.com>
2017-08-13 16:32:06 -0400

    progress on Beacon init

--- config.go
+++ config.go
@@ -3,9 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	// "fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
 )
 
 const (
@@ -62,15 +62,36 @@ func LoadConfig(config []byte) (*Config, error) {
 // InitConfig is used to setup Beacon on first run
 // - It checks to see if there is a beaconrc file with the proper contents
 // - ie. a user and email is configured
-// - if there is no beacon_log.json, create it.
+// - If there is no creds configured, try and retrieve them from git and Prompt ok useage
+//   - if not ok, prompt for custom config mesages. (discuss - maybe we should just only accept git creds?)
+// - if there is no beacon_log.json, prompt if they should create.
 func InitConfig() {
-	_, err := os.Stat("./beaconrdac")
+	_, err := os.Stat("./.beacodlkanrc")
+	// Beacon file Exists
 	if err == nil {
-		fmt.Println("You already have a beaconrc")
-		// TODO: Instruct user they can update it...
+		fmt.Println("You already have a beaconrc! You can update by hand if needed.")
+
+		// beaconrc file does not exist
 	} else if os.IsNotExist(err) {
-		fmt.Println("File does not exist!")
-		// TODO: Prompt user to make one, add credentials etc.
+		getGitUserName := exec.Command("sh", "-c", "git config --get user.name")
+		gitUserName, err := getGitUserName.Output()
+		if err != nil {
+			fmt.Println("No Git user name found", err)
+		}
+
+		getGitEmail := exec.Command("sh", "-c", "git config --get user.email")
+		gitEmail, err := getGitEmail.Output()
+		if err != nil {
+			fmt.Println("No Git email found", err)
+		}
+
+		// Get Git credentials
+		fmt.Println("\nHi, I'm Beacon! 🚨 — I'm here to help you and your team keep in touch about breaking changes.")
+		fmt.Println("I set up a `beaconrc` file in your directory root with your Git information. You can change this whenever you need!")
+		fmt.Println("Thanks for using Beacon and have fun breaking stuff! 🔨")
+
+		fmt.Print("User name: "+string(gitUserName), "Email: "+string(gitEmail), "\n")
+
 	} else {
 		fmt.Println("The stat call to your beaconrc failed", err)
 	}
